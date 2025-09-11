96c4b332196d6d5bc642fb166368b16e1ea25efd
MuhammadJawadSaeed <jawadsaeed917@gmail.com>
2025-09-11 16:22:37 +0500

    implementing image caption generation

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,4 +1,13 @@
-const postModel = require('../models/post.model')
-async function createPostController(req, res) {}
+const postModel = require("../models/post.model");
+const generateCaption = require("../service/ai.service");
+
+async function createPostController(req, res) {
+  const file = req.file;
+  const base64Image = new Buffer.from(file.buffer).toString("base64");
+  const caption = await generateCaption(base64Image);
+  res.json({
+    caption,
+  });
+}
 
 module.exports = createPostController;
---
+++ backend/src/service/ai.service.js
@@ -0,0 +1,30 @@
+const { GoogleGenAI } = require("@google/genai");
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
+
+async function generateCaption(base64ImageFile) {
+  const contents = [
+    {
+      inlineData: {
+        mimeType: "image/jpeg",
+        data: base64ImageFile,
+      },
+    },
+    { text: "Caption this image." },
+  ];
+
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: contents,
+    config: {
+      systemInstruction: `you generate single caption for the image.
+      your caption should be short and concise.
+      you use hashtags and emojis in caption.
+      `,
+    },
+  });
+  return response.text;
+}
+module.exports = generateCaption;
